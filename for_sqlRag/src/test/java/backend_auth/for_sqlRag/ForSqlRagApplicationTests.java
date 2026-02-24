8f71663bd966bb6ece40f663804645206b114f17
Momin2005-ui <msdoo9233@gmail.com>
2026-02-24 23:24:51 +0530

    backend dependencies added

---
+++ for_sqlRag/src/test/java/backend_auth/for_sqlRag/ForSqlRagApplicationTests.java
@@ -0,0 +1,13 @@
+package backend_auth.for_sqlRag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForSqlRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
