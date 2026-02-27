cbbe26a5e3e82a1cc445fa6a46ec1cdafe36df45
Momin2005-ui <msdoo9233@gmail.com>
2026-02-28 00:43:20 +0530

    application.properties model created

--- for_sqlRag/src/main/java/backend_auth/for_sqlRag/Controller/ChatController.java
+++ for_sqlRag/src/main/java/backend_auth/for_sqlRag/Controller/ChatController.java
@@ -1,4 +1,16 @@
 package backend_auth.for_sqlRag.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
 public class ChatController {
+
+    @PostMapping("/history")
+    public String getHistory()
+    {
+        return "history";
+    }
 }
