3cbee572fda9ff07db8bf47b8904ade366e39b9f
Your Name <your-email@example.com>
2026-03-26 14:03:42 +0530

    Removed application.properties from tracking

--- for_sqlRag/src/main/java/backend_auth/for_sqlRag/ForSqlRagApplication.java
+++ for_sqlRag/src/main/java/backend_auth/for_sqlRag/ForSqlRagApplication.java
@@ -1,5 +1,7 @@
 package backend_auth.for_sqlRag;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,8 @@ public class
 ForSqlRagApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ForSqlRagApplication.class, args);
-	}
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    SpringApplication.run(ForSqlRagApplication.class, args);
+}
 
 }
